dba9a499fdb974d10c785d7d0354c8b28a7da3e6
Leonardo Bravo Illanes <leonardbravoillanes@gmail.com>
2014-09-10 01:12:32 -0300

    Install Django

---
+++ install/install.sh
@@ -0,0 +1,8 @@
+sudo apt-get -y install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert libapache2-mod-python
+sudo apt-get -y install libapache2-mod-wsgi
+#git clone https://github.com/django/django.git
+sudo apt-get -y install python-django
+#python-mysqldb #MySQL
+sudo apt-get -y install postgresql python-psycopg2
+sudo apt-get -y install python-django
+easy_install Django
